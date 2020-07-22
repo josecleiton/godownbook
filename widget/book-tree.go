c85468bbef07184cfb7ffe9f0fdfc06048c8f802
José Cleiton <jcleitonbc@gmail.com>
2020-07-22 13:54:03 -0300

    feat(ui): book tree widget

---
+++ widget/book-tree.go
@@ -0,0 +1,39 @@
+package widget
+
+import (
+	ui "github.com/gizak/termui/v3"
+	w "github.com/gizak/termui/v3/widgets"
+)
+
+type BookTree struct {
+	w.Tree
+	i int
+}
+
+type nodeValue string
+
+func (nv nodeValue) String() string {
+	return string(nv)
+}
+
+type BookNode struct {
+	Title  string
+	Childs []string
+}
+
+func NewBookTree(nodes []BookNode) *BookTree {
+	tree := &BookTree{}
+	tree.Tree = *w.NewTree()
+	wNodes := make([]*w.TreeNode, len(nodes))
+	for i, node := range nodes {
+		childs := make([]*w.TreeNode, len(node.Childs))
+		wNodes[i] = &w.TreeNode{Value: nodeValue(node.Title), Nodes: childs}
+		for j, v := range node.Childs {
+			childs[j] = &w.TreeNode{Value: nodeValue(v)}
+		}
+	}
+	tree.TextStyle = ui.NewStyle(ui.ColorGreen)
+	tree.Border = false
+	tree.SetNodes(wNodes)
+	return tree
+}
